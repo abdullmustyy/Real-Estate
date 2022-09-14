fe1e02761ee807c2b3940e5efe9aa150cb9d5ebd
abdullmustyy <abdulmusty03@outlook.com>
2022-09-14 13:23:53 +0100

    Correct Hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
-    <section className="hero relative pt-20 px-4 md:px-0">
+    <section className="hero relative pt-20">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:w-3/5 items-center lg:items-start space-y-4">
-          <h1 className="font-bold text-7xl text-center lg:text-left">
+        <div className="flex flex-col lg:w-3/5 items-start space-y-4 px-4 md:px-0">
+          <h1 className="font-bold text-7xl text-left">
             Turn A House To Your Home
           </h1>
-          <p className="font-normal text-base text-center lg:text-left">
+          <p className="font-normal text-base text-left">
             With over 1 million+ homes availble, we can match you with <br /> a
             house you will want to call home.
           </p>
@@ -19,12 +19,12 @@ export default function Hero() {
           </a>
         </div>
       </div>
-      <div className="flex justify-center lg:justify-end">
+      <div className="flex justify-end">
         <img
           src="https://github.com/abdullmustyy/Real-Estate/blob/main/src/assets/house-1.png?raw=true"
           alt="House 1"
           loading="lazy"
-          className="mt-10 lg:-mt-52 w-5/6"
+          className="mt-10 -mt-1 lg:-mt-52 md:w-5/6"
         />
       </div>
     </section>
